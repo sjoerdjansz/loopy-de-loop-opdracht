ea7f0e2f263d1c471c8c2afabe1eb10db8d6db48
sjoerdjansz <sjoerdjansz@gmail.com>
2025-04-08 16:14:03 +0200

    timestamp opdracht gemaakt

--- loops.js
+++ loops.js
@@ -9,12 +9,12 @@
 
 // Tip: je kunt de .repeat() methode gebruiken om een karakter een n aantal keer te herhalen... Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
-
-const star = "*";
-
-for (let i = 0; i < 6; i++) {
-    console.log(star.repeat(i));
-}
+//
+// const star = "*";
+//
+// for (let i = 0; i < 6; i++) {
+//     console.log(star.repeat(i));
+// }
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de laatste (vijfde) loop het woord "klaar!"
@@ -26,13 +26,13 @@ for (let i = 0; i < 6; i++) {
 // klaar!
 // ==========================================
 //
-for (let i = 0; i < 5; i++) {
-    if (i < 4) {
-        console.log("loop...");
-    } else {
-        console.log("klaar");
-    }
-}
+// for (let i = 0; i < 5; i++) {
+//     if (i < 4) {
+//         console.log("loop...");
+//     } else {
+//         console.log("klaar");
+//     }
+// }
 // ==========================================
 // Opdracht 3. Maak een for-loop die automatisch factuurnummers genereert in het formaat INV-0001, INV-0002, enzovoorts. Zorg ervoor dat je begint bij nummer 1 en dat de nummers oplopen. Er zijn in totaal 8 factuurnummers nodig.
 
@@ -47,10 +47,10 @@ for (let i = 0; i < 5; i++) {
 // 'INV-0008'
 
 // ==========================================
-
-for (let i = 1; i < 9; i++) {
-    console.log(`INV-${String(i).padStart(4, '0')}`);
-}
+//
+// for (let i = 1; i < 9; i++) {
+//     console.log(`INV-${String(i).padStart(4, '0')}`);
+// }
 
 // ==========================================
 // Opdracht 4. Schrijf een for-loop die van 9 tot en met 18 loopt en de uren logt.
@@ -71,6 +71,15 @@ for (let i = 1; i < 9; i++) {
 // 18:00
 // ==========================================
 
+for (let i = 9; i <= 18; i++) {
+    if (i === 12) {
+        console.log(`${i}:00 Koffiepauze`);
+    } else if (i === 17) {
+        console.log((`${i}:00 Bijna klaar`));
+    } else {
+        console.log((`${i}:00`));
+    }
+}
 
 // ==========================================
 // Opdracht 5. Maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
