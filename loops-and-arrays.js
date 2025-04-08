4d55e18ba9be6346c91618ba64e8e6c5a5179d79
sjoerdjansz <sjoerdjansz@gmail.com>
2025-04-08 16:58:05 +0200

    loops and arrays opdrachten afgemaalt

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -92,4 +92,8 @@ console.log(leaveHours);
 
 const productionCodes = [" abC123  ", "  DEF456", "ghi789  ", "JKL012"];
 
+for (let i = 0; i < productionCodes.length; i++) {
+    productionCodes[i] = productionCodes[i].trim(" ").toUpperCase();
+}
 
+console.log(productionCodes);
