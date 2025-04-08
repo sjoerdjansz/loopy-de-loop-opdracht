5a3609d6dd4eabe4b2f6e8f78b8cb69979a2a51c
sjoerdjansz <sjoerdjansz@gmail.com>
2025-04-08 15:59:26 +0200

    eerste paar opdrachten in loops.js gemaakt

--- loops.js
+++ loops.js
@@ -10,6 +10,11 @@
 // Tip: je kunt de .repeat() methode gebruiken om een karakter een n aantal keer te herhalen... Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
 
+const star = "*";
+
+for (let i = 0; i < 6; i++) {
+    console.log(star.repeat(i));
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de laatste (vijfde) loop het woord "klaar!"
@@ -20,8 +25,14 @@
 // loop...
 // klaar!
 // ==========================================
-
-
+//
+for (let i = 0; i < 5; i++) {
+    if (i < 4) {
+        console.log("loop...");
+    } else {
+        console.log("klaar");
+    }
+}
 // ==========================================
 // Opdracht 3. Maak een for-loop die automatisch factuurnummers genereert in het formaat INV-0001, INV-0002, enzovoorts. Zorg ervoor dat je begint bij nummer 1 en dat de nummers oplopen. Er zijn in totaal 8 factuurnummers nodig.
 
@@ -36,6 +47,10 @@
 // 'INV-0008'
 
 // ==========================================
+
+for (let i = 1; i < 9; i++) {
+    console.log(`INV-${String(i).padStart(4, '0')}`);
+}
 
 // ==========================================
 // Opdracht 4. Schrijf een for-loop die van 9 tot en met 18 loopt en de uren logt.
